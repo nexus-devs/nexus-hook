440dbc0e4b8676df9038eb0368762622ea7d8222
Nakroma <tarackobarack@gmail.com>
2017-07-28 18:34:27 +0200

    Added NexusHook header

---
+++ NexusHook/NexusHook.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "Includes.h"
+#include "Dx11Indexes.h"
+#include "SwapChainManager.h"
+#include "VMTHook.h"
+
+
+// Main hooking class
+class NexusHook {
+	public:
+		bool Init();
+		bool HookSwapChain(DWORD newFunc, int index);
+
+		SwapChainManager hMngr;
+
+		const static int iHookNumber = SC_GETLASTPRESENTCOUNT + 1;
+		VMTHook hkHooks[iHookNumber];
+		DWORD oFunctions[iHookNumber];
+};
